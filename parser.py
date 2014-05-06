de8ae78561350da633767e464f940746df07c0bb
b8vrede <bart.vredebregt@gmail.com>
2014-05-06 10:42:00 +0200

    Removed Skeleton code

--- parser.py
+++ parser.py
@@ -149,28 +149,4 @@ def get_bwb_name_dict(XML=get_bwb_info()):
     return dict
         
 
-    
-# Skeleton source: http://code.activestate.com/recipes/528877-skeleton-script-hello-world/
-if __name__ == '__main__':
-    try:
-        
-        parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), usage=globals()['__doc__'], version='$Id$')
-        parser.add_option ('-v', '--verbose', action='store_true', default=False, help='verbose output')
-        (options, args) = parser.parse_args()
-        #if len(args) < 1:
-        #    parser.error ('missing argument')
-        if options.verbose: print time.asctime()
-        main()
-        if options.verbose: print time.asctime()
-        if options.verbose: print 'TOTAL TIME IN MINUTES:',
-        if options.verbose: print (time.time() - start_time) / 60.0
-        sys.exit(0)
-    except KeyboardInterrupt, e: # Ctrl-C
-        raise e
-    except SystemExit, e: # sys.exit()
-        raise e
-    except Exception, e:
-        print 'ERROR, UNEXPECTED EXCEPTION'
-        print str(e)
-        traceback.print_exc()
-        os._exit(1)
+main()
