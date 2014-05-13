8f0582b5a310ffec602444770bcc4914fe994751
b8vrede <bart.vredebregt@gmail.com>
2014-05-13 13:11:54 +0200

    added Random function to eval.py
    Using -r N, N random references are selected from all avaible documents.
    (useful when using large datasets)

--- eval.py
+++ eval.py
@@ -1,5 +1,7 @@
 import glob
 import os
+import argparse
+import random
 import xml.etree.ElementTree as ET
 
 def read_ecli_files():
@@ -19,6 +21,16 @@ def find_ref(file):
     
     return refs
 
+def fetch_random_refs(fileList, n):
+    allRefs = []
+    for file in fileList:
+        allRefs.extend(find_ref(file))
+    
+    if allRefs is not None and n > 0 and n < len(allRefs):
+        allRefs = random.sample(allRefs, n)
+    
+    return allRefs
+    
 def eval_refs(refs):
     global positive, negative
     
@@ -38,20 +50,36 @@ def eval_refs(refs):
             positive += 1
         elif answer.lower() == "n":
             negative += 1
+# Main function
+if __name__ == '__main__':
+    positive = 0
+    negative = 0
+    
+    # Parse the arguments
+    parser = argparse.ArgumentParser(description='Tool for evaluation')
+    parser.add_argument("-r", "--random",
+                        action="store", type=int, metavar="X", dest="random", default=None,
+                        help="Creates a random sample of size X from the list of selected ECLIs")                    
+    args = parser.parse_args()
+    
+    fileList = read_ecli_files()
+    
+    if args.random > 0:
+        refs = fetch_random_refs(fileList, args.random)
+        print("+---------------------------------------------------+\n|\tFile:\t- (Random from {} file(s))".format(len(fileList)))
+        print("|\tRefs: \t{}".format(len(refs)))
+        eval_refs(refs)
+        total = positive + negative
+        print "P: {} ({:.2%}) \nN: {} ({:.2%})".format(positive, float(positive)/total, negative, float(negative)/total)
+    else:
+        for file in fileList:
+            print("+---------------------------------------------------+\n|\tFile:\t{}".format(file))
+            refs = find_ref(file)
 
-positive = 0
-negative = 0
+            print("|\tRefs: \t{}".format(len(refs)))
 
-fileList = read_ecli_files()
+            eval_refs(refs)
 
-for file in fileList:
-    print("+---------------------------------------------------+\n|\tFile:\t{}".format(file))
-    refs = find_ref(file)
+            total = positive + negative
 
-    print("|\tRefs: \t{}".format(len(refs)))
-
-    eval_refs(refs)
-
-    total = positive + negative
-
-    print "P: {} ({:.2%}) \nN: {} ({:.2%})".format(positive, float(positive)/total, negative, float(negative)/total)
+            print "P: {} ({:.2%}) \nN: {} ({:.2%})".format(positive, float(positive)/total, negative, float(negative)/total)
